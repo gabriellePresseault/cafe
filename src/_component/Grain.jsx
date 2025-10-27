6dc6b58757718b1ff6835b178f14e9ce827dbcc7
Your Name <2240517@cstj.qc.ca>
2025-10-27 14:23:58 -0400

    grain1

--- src/_component/Grain.jsx
+++ src/_component/Grain.jsx
@@ -1,9 +1,14 @@
+'use client'
+import { useState } from "react";
 import "./Grain.css"
 
 const Grain = () => {
 
+    
     return (
-        <div className="GrainZone"></div>
+        <div className="GrainZone">
+            <button onClick={augGrains}></button>
+        </div>
     );
 }  
 
--- src/_component/Machine.jsx
+++ src/_component/Machine.jsx
@@ -2,11 +2,20 @@ import Grain from "./Grain";
 import Cafe from "./Cafe";
 import Creme from "./Creme";
 import Temperature from "./Temperature";
+import { useState } from "react";
+
 
 const Machine = () => {
+    const [nbgrain, setNbGrain]=useState();
+
+    const augGrains=()=>{
+        setNbGrain(nbgrain+1)
+    }
+
+    console.log(nbgrain)
     return (
         <div>
-            <Grain/>
+            <Grain augGrains={augGrains}/>
             <Temperature/>
             <Cafe/>
             <Creme/>
