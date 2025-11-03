136d18ede6bf92bf246ccc58b4e746bd49a153f2
Your Name <2240517@cstj.qc.ca>
2025-11-03 10:32:54 -0500

    changer le sprite

---
+++ src/_component/Boutons.jsx
@@ -0,0 +1,10 @@
+// const Boutons =()=>{
+//     return(
+//         <div>
+//             <button><img src="/img/bouton1.png" alt="" /></button>
+//             <button><img src="/img/bouton1.png" alt="" /></button>
+//             <button><img src="/img/bouton1.png" alt="" /></button>
+//         </div>
+//     )
+// }
+// export default Boutons;
--- src/_component/Creme.jsx
+++ src/_component/Creme.jsx
@@ -1,9 +1,11 @@
+import Boutons from "./Boutons";
 import "./Creme.css"
 
 const Creme = () => {
 
     return (
         <div className="CremeZone"></div>
+        
     );
 }
 
