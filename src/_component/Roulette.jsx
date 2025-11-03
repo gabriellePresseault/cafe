5782964064a85ddf4b92b28e6060444552c76fb7
AlisonSelway <2378161@cstj.qc.ca>
2025-11-03 08:30:14 -0500

    Modification de l'apparence de la machine

--- src/_component/Roulette.jsx
+++ src/_component/Roulette.jsx
@@ -1,42 +1,45 @@
 'use client';
 
-import "./Roulette.css"
-
+import "./Roulette.css";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { Draggable } from "gsap/Draggable";
-import { useRef } from "react";
 
 gsap.registerPlugin(Draggable);
 
 const Roulette = () => {
+  const knobRef = useRef(null);
+  const [rotation, setRotation] = useState(0);
 
-console.clear();
+  useEffect(() => {
+    if (!knobRef.current) return;
+
+    const draggable = Draggable.create(knobRef.current, {
+      type: "rotation",
+      onDrag: function () {
+        setRotation(this.rotation); 
+      },
+    })[0];
+
+    return () => draggable.kill(); 
+  }, []);
 
 
-//it doesn't get much easier than this ;)
-const draggable = Draggable.create("#knob", {
-  type: "rotation",
-  onDrag: function() {
-    console.log(this.rotation)
-  }
-});
-
- console.log(Draggable.get("#knob").rotation, "from the Draggable")
-
-    return (
-        <div className="Roulette">
-            <svg className="contenue" id="knob" width="280" height="280">
-                <circle
-                    cx="140"
-                    cy="140"
-                    r="90"
-                    fill="gray"
-                    stroke="lightgray"
-                    stroke-width="5"
-                />
-            </svg>
-        </div>
-    );
-}
+    const rotKnob = Math.round(rotation);
+  return (
+    <div className="Roulette">
+      <svg className="contenue" ref={knobRef} id="knob" width="280" height="280">
+        <circle
+          cx="140"
+          cy="140"
+          r="90"
+          fill="gray"
+          stroke="lightgray"
+          strokeWidth="5"
+        />
+      </svg>
+    </div>
+  );
+};
 
 export default Roulette;
